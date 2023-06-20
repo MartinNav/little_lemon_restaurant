f3cdee0e6391b958ef463ef08d565b7e52931b40
unknown <martinnavratilu@gmail.com>
2023-06-20 11:32:37 +0200

    added models

--- littleLemon/littleLemon/settings.py
+++ littleLemon/littleLemon/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -37,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'restaurant',
     
 ]
 
@@ -73,14 +75,27 @@ WSGI_APPLICATION = 'littleLemon.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
-
+"""
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
+"""
+DATABASES = {
+    'default': {
+ 'ENGINE': 'django.db.backends.mysql',
+ 'NAME': 'LittleLemon',
+ 'USER': 'root',
+ 'PASSWORD': '',
+ 'HOST': '127.0.0.1',
+ 'PORT': '3306',
+ 'OPTIONS': {
+ 'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"
+ }
+ }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
@@ -116,7 +131,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+#STATIC_ROOT = os.path.join(BASE_DIR, 'restaurant/static')
+#STATICFILES_DIRS =( os.path.join(STATIC_ROOT, 'restaurant/'),)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
--- littleLemon/restaurant/views.py
+++ littleLemon/restaurant/views.py
@@ -1,5 +1,13 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, HttpResponse
+import os
 # Create your views here.
 def index(request):
   return render(request, 'index.html', {})
+
+def staticfiles(request,name):
+  print(name)
+  try:
+    with open(os.path.join(os.path.dirname(__file__),'static/restaurant/'+name),'rb') as f:
+      return HttpResponse(f.read(),content_type="image/jpeg")
+  except IOError:
+    return HttpResponse(request,code=404)
