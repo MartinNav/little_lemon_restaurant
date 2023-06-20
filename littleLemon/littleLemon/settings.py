71fde19ce0922b36a50f173a29c0ed0c2999cb61
unknown <martinnavratilu@gmail.com>
2023-06-20 18:13:54 +0200

    added browsable api

--- littleLemon/littleLemon/settings.py
+++ littleLemon/littleLemon/settings.py
@@ -73,6 +73,15 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'littleLemon.wsgi.application'
 
+REST_FRAMEWORK = {
+  'DEFAULT_PARSER_CLASSES': (
+    'rest_framework.parsers.JSONParser',
+  ),
+    'DEFAULT_RENDERER_CLASSES': (
+    'rest_framework.renderers.JSONRenderer',
+    'rest_framework.renderers.BrowsableAPIRenderer',
+  ),
+}
 
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
